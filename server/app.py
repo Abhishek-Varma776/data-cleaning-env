9b0558a58cdbc6066678d73d918ba48a595581b4
Pujala Shiva Naga Abhishek Varma <pujalasnabhishek@gmail.com>
2026-04-08 20:53:23 +0530

    fix openenv validation issues

--- server/app.py
+++ server/app.py
@@ -107,3 +107,10 @@ def state():
         "done":      s.done,
     }
 
+def main():
+    import uvicorn
+    uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
+
+
+if __name__ == "__main__":
+    main()
